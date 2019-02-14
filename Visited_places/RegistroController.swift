169425e688ea6b8f118f1bc37fef907769588559
MrCocomass <alumnos@409-09.local>
2019-02-14 14:29:29 +0100

    jueves commit

--- Visited_places/RegistroController.swift
+++ Visited_places/RegistroController.swift
@@ -1,22 +1,34 @@
-//
-//  RegistroController.swift
-//  Visited_places
-//
-//  Created by alumnos on 12/2/19.
-//  Copyright © 2019 alumnos. All rights reserved.
-//
 
+import Foundation
 import UIKit
 import Alamofire
 
 class RegistroController: UIViewController {
 
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
-
+    
+//    @IBOutlet weak var userText: UITextField!
+//    @IBOutlet weak var passText: UITextField!
+//    @IBOutlet weak var repeatpassText: UITextField!
+//    @IBOutlet weak var emailText: UITextField!
+//
+////    @IBAction func registerButton(_ sender: Any) {
+////        let password = passText.text
+////        let confirmapss = repeatpassText.text
+////        if password == confirmapss
+////        {
+//////            postPetition()
+//////        }
+//////        else
+//////        {
+//////            self.alert(title:"Caution", message: "Diferent password")
+////        }
+////    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
