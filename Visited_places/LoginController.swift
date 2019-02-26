9d5fd9e4c5c9d13f68406eafdb771c0e92479858
MrCocomass <daniel_miranda_apps1ma1718@cev.com>
2019-02-26 19:24:59 +0100

    26_02_ commit

---
+++ Visited_places/LoginController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginRegisterViewController.swift
+//  Visited_places
+//
+//  Created by Daniel Miranda on 25/02/2019.
+//  Copyright © 2019 alumnos. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
